168213d774706b5d40533232371c126724143fec
jilongliang <jilongliang@sina.com>
2020-08-09 19:07:38 +0800

    maven project 改成 gradle project

---
+++ src/main/java/com/flong/springboot/Application.java
@@ -0,0 +1,21 @@
+package com.flong.springboot;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+/**
+ * 参考文章：https://www.cnblogs.com/qwlscn/p/11490071.html
+ */
+@SpringBootApplication
+@ComponentScan("com.flong.springboot")  //扫描com.flong文件目录下
+@MapperScan(basePackages = {"com.flong.springboot"}) //扫描DAO
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
--- src/main/java/com/flong/springboot/modules/mapper/UserMapper.java
+++ src/main/java/com/flong/springboot/modules/mapper/UserMapper.java
@@ -1,6 +1,5 @@
 package com.flong.springboot.modules.mapper;
 
-
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.flong.springboot.modules.entity.User;
 
